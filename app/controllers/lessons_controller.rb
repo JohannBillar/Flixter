ed6d2127f0a8de7e1887866eda7687613817816d
JohannBillar <jwbillar@gmail.com>
2015-11-29 02:12:19 +0000

    Required user enrollment to view lessons.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,15 +1,22 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :confirm_user_enrollment, :only => [:show]
 	
 	def show
-		# if current_lesson.blank?
-		# 	render :text => "Not found", :status => :not_found
-		# end
 	end
 
 	private
-	helper_method :current_lesson
-	def current_lesson
-		@current_lesson ||= Lesson.find(params[:id])
-	end
+
+	def confirm_user_enrollment
+		this_course = current_lesson.section.course
+		if !current_user.enrolled_in?(this_course)
+      redirect_to course_path(this_course), :alert => 'Please enroll to see lessons'
+    end	
+  end
+
+  helper_method :current_lesson
+  def current_lesson
+    @current_lesson ||= Lesson.find(params[:id])
+  end
 
 end
