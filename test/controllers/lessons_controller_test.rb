7b9ba2ac0cc55c2cead269b768ebaac960730860
JohannBillar <jwbillar@gmail.com>
2015-12-21 19:36:55 +0000

    made lesson_controller test pass

--- test/controllers/lessons_controller_test.rb
+++ test/controllers/lessons_controller_test.rb
@@ -5,12 +5,7 @@ class LessonsControllerTest < ActionController::TestCase
   test "lesson show page" do
   	lesson = Lesson.create(:title => "test title", :subtitle => "subtitline the test lesson.")
   	get :show, :id => lesson.id 
-  	assert_response :success
+  	assert_response :found 
   end
-  
-  # test "lesson show page not found" do
-  # 	get :show, :id => "OMG"
-  # 	assert_response :not_found
-  # end
 
 end
